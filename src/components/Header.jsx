b8e79d673a23a81afa61bc9daaa464aaeb072543
Rizolis <vitor.correa@estudante.ifms.edu.br>
2024-08-14 16:06:08 -0400

    aula dia 14/08
    terminar carrosel

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom"
+
+export default function Header(){
+    return(
+        <>
+        <nav>
+            <ul>
+                <Link to="/Home">
+                    <li>Home</li>
+                </Link>
+                <Link to="/Ofertas">
+                    <li>Ofertas</li>
+                </Link>
+            </ul>
+        </nav>
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Home'
+import Ofertas from './Ofertas'
+import Header from './components/Header'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+    <BrowserRouter>
+         <Routes>
+         <Route path="/" element={<Home/>}/>
+         <Route path="/Ofertas" element={<Ofertas/>}/>
+         <Route path="components" element={Component}/>
+         </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
 
