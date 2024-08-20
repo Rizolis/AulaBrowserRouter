9adf493e5b188a9f9443c4d384f0b0a8252eca9c
Rizolis <vitor.correa@estudante.ifms.edu.br>
2024-08-20 17:29:45 -0400

    terminar 20/08
    terminar 20

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,10 +1,34 @@
 import Header from "../components/Header";
+import{Carousel} from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 export default function Home(){
 return(
     <>
     <Header/>
     <p>Minha Home</p>
-    </>
+    
+
+<Carousel
+infiniteLoop
+useKeyboardArrows
+autoPlay
+showArrows={true}
+showStatus={false}
+showThumbs={false}
+dynamicHeight
+>
+<div>
+    <img src="" alt="Slide 1" />
+</div>
+<div>
+    <img src="" alt="Slide 1" />
+</div>
+<div>
+    <img src="" alt="Slide 1" />
+</div>
+
+</Carousel>
+</>
 );
 }
